bac6bf5efcb75b1aec9ca534259ddf07a610e70f
buzzni <buzzni@sally.local>
2018-07-18 15:30:31 +0900

    remove inproper characters

--- build/index.js
+++ build/index.js
@@ -1,33 +1,37 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Namer = (function () {
+var _ = require("lodash");
+var Namer = /** @class */ (function () {
     function Namer() {
         this.firstNames = [
             44608, 51060, 48149, 52572, 51221, 44053, 51312, 50980, 51109, 51076,
             50724, 54620, 49888, 49436, 44428, 54889, 50504, 49569, 47448, 54861,
             51204, 47928, 49552, 50577
         ];
-        this.startCharCode = 44032;
-        this.endCharCode = 55203;
+        this.firstCount = this.firstNames.length;
+        this.acceptableCho = [0, 2, 3, 5, 6, 7, 9, 11, 12, 14, 15, 16, 17, 18];
+        this.acceptableJung = [0, 1, 4, 5, 6, 8, 9, 11, 14, 17, 18, 19, 20];
+        this.acceptableJong = [0, 1, 4, 8, 16, 17, 19, 21];
     }
     Namer.prototype.getRandomInt = function (min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
     };
-    Namer.prototype.getFirstName = function () {
-        var index = this.getRandomInt(0, this.firstNames.length);
+    Namer.prototype.getFirstChar = function () {
+        var index = this.getRandomInt(0, this.firstCount);
         return String.fromCharCode(this.firstNames[index]);
     };
-    Namer.prototype.getMidName = function () {
-        var charCode = this.getRandomInt(this.startCharCode, this.endCharCode);
-        return String.fromCharCode(charCode);
-    };
-    Namer.prototype.getLastName = function () {
-        var charCode = this.getRandomInt(this.startCharCode, this.endCharCode);
-        return String.fromCharCode(charCode);
+    Namer.prototype.getRandomChar = function () {
+        var base = 0xAC00;
+        var jong = base + _.sample(this.acceptableJong);
+        console.debug('jong', String.fromCharCode(jong));
+        var jung = jong + 28 * _.sample(this.acceptableJung);
+        console.debug('jung', String.fromCharCode(jung));
+        var cho = jung + 28 * 21 * _.sample(this.acceptableCho);
+        console.debug('cho', String.fromCharCode(cho));
+        return String.fromCharCode(cho);
     };
     Namer.prototype.generate = function () {
-        var unicodeName = this.getFirstName() + this.getMidName() + this.getLastName();
-        return unicodeName;
+        return "" + this.getFirstChar() + this.getRandomChar() + this.getRandomChar();
     };
     return Namer;
 }());
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+import * as _ from 'lodash';
+
 export default class Namer {
   private firstNames: number[];
-  private startCharCode: number;
-  private endCharCode: number;
+  private firstCount: number;
+  private acceptableJong: number[];
+  private acceptableJung: number[];
+  private acceptableCho: number[];
 
   constructor() {
     this.firstNames = [
@@ -9,33 +13,35 @@ export default class Namer {
       50724, 54620, 49888, 49436, 44428, 54889, 50504, 49569, 47448, 54861,
       51204, 47928, 49552, 50577
     ];
+    this.firstCount = this.firstNames.length;
 
-    this.startCharCode = 44032;
-    this.endCharCode = 55203;
+    this.acceptableCho = [ 0, 2, 3, 5, 6, 7, 9, 11, 12, 14, 15, 16, 17, 18 ];
+    this.acceptableJung = [ 0, 1, 4, 5, 6, 8, 9, 11, 14, 17, 18, 19, 20 ];
+    this.acceptableJong = [ 0, 1, 4, 8, 16, 17, 19, 21 ];
   }
 
   private getRandomInt(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
-  private getFirstName(): string {
-    let index: number = this.getRandomInt(0, this.firstNames.length);
+  private getFirstChar(): string {
+    const index: number = this.getRandomInt(0, this.firstCount);
     return String.fromCharCode(this.firstNames[index]);
   }
 
-  private getMidName(): string {
-    let charCode: number = this.getRandomInt(this.startCharCode, this.endCharCode);
-    return String.fromCharCode(charCode);
-  }
-
-  private getLastName(): string {
-    let charCode: number = this.getRandomInt(this.startCharCode, this.endCharCode);
-    return String.fromCharCode(charCode);
+  private getRandomChar(): string {
+    const base = 0xAC00;
+    const jong = base + _.sample(this.acceptableJong);
+    console.debug('jong', String.fromCharCode(jong));
+    const jung = jong + 28 * _.sample(this.acceptableJung);
+    console.debug('jung', String.fromCharCode(jung));
+    const cho = jung + 28 * 21 * _.sample(this.acceptableCho);
+    console.debug('cho', String.fromCharCode(cho));
+    return String.fromCharCode(cho);
   }
 
   public generate(): string {
-    let unicodeName: string = this.getFirstName() + this.getMidName() + this.getLastName();
-    return unicodeName;
+    return `${this.getFirstChar()}${this.getRandomChar()}${this.getRandomChar()}`;
   }
 }
 
